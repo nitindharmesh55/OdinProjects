1588b636e4e9075d012f30102e3792d3b94c7100
Nitin <nitindharmesh01@gmail.com>
2025-06-01 10:17:30 +0530

    new Code

---
+++ Javascript/script.js
@@ -0,0 +1,24 @@
+let name = "john"
+let surname = "doe"
+console.log(name);
+console.log(surname);
+
+let age = 11;
+console.log(age);
+age = 54;
+console.log(age);
+
+console.log(23 + 97);
+console.log((4 + 6 + 9) / 77)
+
+let a = 10;
+console.log(a);
+a = 25;
+console.log(a)
+let b =  7 * a;
+console.log(b);
+
+const max = 57;
+const actual = max - 13;
+const precentage  = actual / max;
+console.log(precentage);
